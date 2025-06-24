55d3ccdc640f4cf88a8651ae265b60b0327302c1
Doom9527 <wpan865@gmail.com>
2025-06-24 09:41:12 +0000

    deploy: 32afe2d1661504f9427d32e6397402c716551634

---
+++ en/assets/js/acecf23e.b1931848.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/aristotle-webservice/en/blog","blogTitle":"Blog","authorsListPath":"/aristotle-webservice/en/blog/authors"}')}}]);
---
+++ en/assets/js/ce7cb3a4.2c8a7660.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[227],{4651:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/aristotle-webservice/en/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
